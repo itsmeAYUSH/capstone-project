95144ff4fdb0abf0761ea9240e459e516d50853e
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-05-19 13:02:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+
+function App() {
+  return <></>;
+}
+
+export default App;
